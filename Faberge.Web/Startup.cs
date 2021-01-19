d0e09ac2d4a5814d31cf68a6161ebca49f00e082
Max <lyuban.maks@gmail.com>
2021-01-19 10:01:01 +0200

    Add project files.

---
+++ Faberge.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Faberge.Web.Startup))]
+namespace Faberge.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
