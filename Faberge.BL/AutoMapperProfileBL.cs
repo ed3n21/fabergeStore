e0b0ce42e5981cb7fdd45e584092a8cd21a6e698
Max <lyuban.maks@gmail.com>
2021-01-19 12:03:33 +0200

    Add generic service and BL mapper

---
+++ Faberge.BL/AutoMapperProfileBL.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Faberge.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faberge.BL
+{
+    public class AutoMapperProfileBL : Profile
+    {
+        public AutoMapperProfileBL()
+        {
+            CreateMap<Product, ProductBL>().ReverseMap();
+            CreateMap<Catalog, CatalogBL>().ReverseMap();
+            CreateMap<Order, OrderBL>().ReverseMap();
+        }
+    }
+}
---
+++ Faberge.BL/Services/GenericService.cs
@@ -0,0 +1,63 @@
+﻿using Faberge.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faberge.BL.Services
+{
+    public interface IGenericService<ModelBL> where ModelBL : class
+    {
+        ModelBL Get(int id);
+        IEnumerable<ModelBL> Get();
+        void Create(ModelBL model);
+        void Edit(ModelBL model);
+        void Delete(int id);
+    }
+
+    public abstract class GenericService<ModelBL, ModelDAL> : IGenericService<ModelBL>
+        where ModelBL : class
+        where ModelDAL : class
+    {
+        private readonly IGenericRepository<ModelDAL> _repository;
+
+        public GenericService()
+        {
+            _repository = new GenericRepository<ModelDAL>();
+        }
+
+        public void Create(ModelBL modelBL)
+        {
+            var model = Map(modelBL);
+            _repository.Create(model);
+        }
+
+        public void Delete(int id)
+        {
+            _repository.Delete(id);
+        }
+
+        public void Edit(ModelBL modelBL)
+        {
+            var model = Map(modelBL);
+            _repository.Edit(model);
+        }
+
+        public ModelBL Get(int id)
+        {
+            return Map(_repository.Get(id));
+        }
+
+        public IEnumerable<ModelBL> Get()
+        {
+            return Map(_repository.Get());
+        }
+
+        public abstract ModelDAL Map(ModelBL model);
+        public abstract ModelBL Map(ModelDAL model);
+
+        public abstract IEnumerable<ModelDAL> Map(IEnumerable<ModelBL> models);
+        public abstract IEnumerable<ModelBL> Map(IEnumerable<ModelDAL> models);
+    }
+}
