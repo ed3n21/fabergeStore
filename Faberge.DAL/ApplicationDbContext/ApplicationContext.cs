bb3e99b9fa304918f43a5fb7cf62bf7184418242
Max <lyuban.maks@gmail.com>
2021-01-19 11:13:36 +0200

    Add DAL models and context

---
+++ Faberge.DAL/ApplicationDbContext/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using Faberge.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faberge.DAL.ApplicationDbContext
+{
+    class ApplicationContext : DbContext
+    {
+        public ApplicationContext() : base("FabergeStore") { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Catalog> Catalogs { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
---
+++ Faberge.DAL/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faberge.DAL.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public string ImgSrc { get; set; }
+
+        public int CatalogId { get; set; }
+        public Catalog Catalog { get; set; }
+    }
+}
